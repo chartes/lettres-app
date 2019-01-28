dd2662c20820c20bce86a1ec403fc6a4a18d5507
Morgan Legal <morgan.legal@polenordstudio.fr>
2019-01-28 11:44:29 +0100

    test csrf

--- app/client/src/modules/http-common.js
+++ app/client/src/modules/http-common.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import { getCookie } from './cookies-helpers'
 const _baseApiURL = `/lettres/api/1.0`;
 const _baseAppURL = `/lettres`;
 
@@ -8,9 +8,13 @@ export const baseApiURL = _baseApiURL;
 export const baseAppURL = _baseAppURL;
 export const http = axios.create({
   baseURL: _baseApiURL,
-  /*headers: {
-    Authorization: 'Bearer {token}'
-  }*/
+  headers: {
+    //Authorization: 'Bearer {token}'
+    //'Authorization': 'Bearer ' + getCookie('csrf_access_token'),
+    //'X-CSRFToken': getCookie('csrf_access_token'),
+    'X-CSRF-Token': getCookie('csrf_access_token'),
+    //'CSRF': getCookie('csrf_access_token'),
+  }
 });
 
 
