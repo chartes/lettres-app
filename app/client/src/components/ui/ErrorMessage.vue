c2f3d24b508e7c16540c9d6fcf6238203899a804
Morgan Legal <morgan.legal@polenordstudio.fr>
2019-02-11 14:17:17 +0100

    add error message component

---
+++ app/client/src/components/ui/ErrorMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="error" class="notification is-danger">
+    {{error}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ErrorMessage',
+    props: {
+      error: { type: String, default: null }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
