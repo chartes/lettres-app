81f77d98776a7ff3971a9bfd11d83b4569862703
MrGecko <vrlitwhai.kridanik@gmail.com>
2019-03-22 15:49:37 +0100

    prep for the coming manifest template stuff

--- app/api/manifest/routes.py
+++ app/api/manifest/routes.py
@@ -51,6 +51,20 @@ HEADERS = {"Access-Control-Allow-Origin": "*",
 #        )
 #
 
+@api_bp.route("/api/<api_version>/iiif/editor/manifests/<witness_id>/template")
+def get_manifest_template(api_version, witness_id):
+    witness = Witness.query.filter(Witness.id == witness_id).first()
+    host = request.host_url
+    # TODO : faire un manifest template (avec les bonnes urls dedans quand même)
+    manifest, manifest_url = current_app.manifest_factory.make_manifest(host, witness)
+
+    response = Response(
+        json.dumps(manifest, indent=2, ensure_ascii=False),
+        content_type=CONTENT_TYPE,
+        headers=HEADERS,
+    )
+    return response, 200
+
 @api_bp.route("/api/<api_version>/iiif/documents/<doc_id>/collection/default")
 def get_collection(api_version, doc_id):
     document = Document.query.filter(Document.id == doc_id).first()
@@ -64,8 +78,31 @@ def get_collection(api_version, doc_id):
         return response, 200
     else:
         return JSONAPIResponseFactory.make_errors_response(
-               {"status": 404, "title": "Document %s does not exist" % doc_id}, status=404
+            {"status": 404, "title": "Document %s does not exist" % doc_id}, status=404
         )
+
+def upload_manifest(filename):
+    SFTP_CONFIG = {
+        "host": current_app.config.get("SFTP_IIIF_HOST", ""),
+        "username": current_app.config.get("SFTP_IIIF_USERNAME", ""),
+        "password": current_app.config.get("SFTP_IIIF_PASSWORD", ""),
+        "default_path": current_app.config.get("SFTP_IIIF_DEFAULT_MANIFEST_PATH", "/srv/manifests")
+    }
+    # print(SFTP_CONFIG)
+    with pysftp.Connection(**SFTP_CONFIG) as srv:
+        srv.put(filename)
+
+
+def upload_collection(filename):
+    SFTP_CONFIG = {
+        "host": current_app.config.get("SFTP_IIIF_HOST", ""),
+        "username": current_app.config.get("SFTP_IIIF_USERNAME", ""),
+        "password": current_app.config.get("SFTP_IIIF_PASSWORD", ""),
+        "default_path": current_app.config.get("SFTP_IIIF_DEFAULT_COLLECTION_PATH", "/srv/collections")
+    }
+    # print(SFTP_CONFIG)
+    with pysftp.Connection(**SFTP_CONFIG) as srv:
+        srv.put(filename)
 
 
 @api_require_roles("contributor")
@@ -133,29 +170,6 @@ def iiif_editor_manifest(api_version, manifest_id=None):
 
     return JSONAPIResponseFactory.make_response(data), code
 
-
-def upload_manifest(filename):
-    SFTP_CONFIG = {
-        "host" : current_app.config.get("SFTP_IIIF_HOST", ""),
-        "username": current_app.config.get("SFTP_IIIF_USERNAME", ""),
-        "password": current_app.config.get("SFTP_IIIF_PASSWORD", ""),
-        "default_path": current_app.config.get("SFTP_IIIF_DEFAULT_MANIFEST_PATH", "/srv/manifests")
-    }
-    #print(SFTP_CONFIG)
-    with pysftp.Connection(**SFTP_CONFIG) as srv:
-        srv.put(filename)
-
-
-def upload_collection(filename):
-    SFTP_CONFIG = {
-        "host": current_app.config.get("SFTP_IIIF_HOST", ""),
-        "username": current_app.config.get("SFTP_IIIF_USERNAME", ""),
-        "password": current_app.config.get("SFTP_IIIF_PASSWORD", ""),
-        "default_path": current_app.config.get("SFTP_IIIF_DEFAULT_COLLECTION_PATH", "/srv/collections")
-    }
-    # print(SFTP_CONFIG)
-    with pysftp.Connection(**SFTP_CONFIG) as srv:
-        srv.put(filename)
 
 @api_require_roles("contributor")
 @api_bp.route("/api/<api_version>/iiif/witnesses/<witness_id>/manifest", methods=('PUT', ))
