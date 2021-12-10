847b074b7538f5edfc536707859f863a805e2626
Julien Pilla <vrlitwhai.kridanik@gmail.com>
2021-12-10 15:14:34 +0100

    launcher

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,9 @@
 from app import create_app
 
-flask_app = create_app()
 
 if __name__ == "__main__":
+    flask_app = create_app(with_hardcoded_prefix=True)
     flask_app.run(debug=True, port=5004, host='0.0.0.0')
+else:
+    flask_app = create_app()
+
